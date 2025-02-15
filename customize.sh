39301bd4df5a776d008717b9982d146e89db7ca6
Esa Varemo <git@esav.fi>
2025-02-15 20:04:32 +0200

    Add graphical console

--- customize.sh
+++ customize.sh
@@ -21,5 +21,5 @@ podman run --rm -i \
 
 podman run --security-opt label=disable --pull=always --rm -v .:/data -w /data \
     quay.io/coreos/coreos-installer:release \
-    iso customize --dest-ignition config.ign --dest-device /dev/sda --dest-console ttyS0,115200n8 \
+    iso customize --dest-ignition config.ign --dest-device /dev/sda --dest-console=tty0 --dest-console ttyS0,115200n8 \
     -o ignited.iso fcos-custom-live-iso.x86_64.iso
