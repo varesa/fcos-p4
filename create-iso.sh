af5308786e11c930f5beb6ba8814343595ef3133
Esa Varemo <git@esav.fi>
2025-02-15 23:14:40 +0200

    Set a better name for the ostree deployment and clean up large intermediate products

--- create-iso.sh
+++ create-iso.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"/build
+HASH="$(git rev-parse HEAD)"
 
-skopeo copy containers-storage:registry.acl.fi/public/fcos:latest oci-archive:fcos-custom
-sudo ../custom-coreos-disk-images.sh --ociarchive fcos-custom --platforms live
-
+skopeo copy containers-storage:registry.acl.fi/public/fcos:latest "oci-archive:install-$HASH"
+sudo ../custom-coreos-disk-images.sh --ociarchive "install-$HASH" --platforms live
+rm "install-$HASH" install-*-initramfs.x86_64.img install-*-kernel.x86_64  install-*-rootfs.x86_64.img
