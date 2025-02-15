715cf0b31483d4f0ddbeaee7437cfd1aa70a6cd2
Esa Varemo <git@esav.fi>
2025-02-15 18:24:02 +0200

    Add build-and-publish.sh

---
+++ build-and-publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+TMP="tmp-fcos-image-build"
+REMOTE="registry.acl.fi/public/fcos"
+DATE="$(date +%Y%m%d-%H%M%S)"
+HASH="$(git rev-parse HEAD)"
+
+if [ "$(git status --porcelain)" == "" ]; then
+    CLEAN=true
+else
+    CLEAN=false
+fi
+
+podman build ${PODMAN_BUILD_OPTS:-} -t "$TMP" .
+
+[ "$CLEAN" == "true" ] && podman tag "$TMP" "$REMOTE:$DATE-$HASH"
+podman tag "$TMP" "$REMOTE:$DATE"
+podman tag "$TMP" "$REMOTE:latest"
+
+[ "$CLEAN" == "true" ] && podman push "$REMOTE:$DATE-$HASH"
+podman push "$REMOTE:$DATE"
+podman push "$REMOTE:latest"
