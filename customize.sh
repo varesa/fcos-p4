a604a348df2f00ff5b72df949d86b8152ab2df01
Esa Varemo <git@esav.fi>
2025-02-16 14:21:45 +0200

    Make customize.sh accept multiple butane configs

--- customize.sh
+++ customize.sh
@@ -2,24 +2,36 @@
 
 set -euo pipefail
 
-CONFIG="${1:-}"
-
-if [[ -z "$CONFIG" || ! -f "$CONFIG" ]]; then
-    echo "Usage: $0 <config path>"
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 <config1.bu> [config2.bu ...]"
     exit 1
-else
-    CONFIG="$(realpath "$CONFIG")"
 fi
 
+
+IGN_FILES=()
+
+for CONFIG in "$@"; do
+    if [[ ! -f "$CONFIG" ]]; then
+        echo "Error: File '$CONFIG' not found!"
+        exit 1
+    fi
+
+    BASENAME=$(basename "$CONFIG" .bu)
+    IGN_FILE="${BASENAME}.ign"
+
+    podman run --rm -i quay.io/coreos/butane:release --pretty --strict < "$CONFIG" > build/"$IGN_FILE"
+    IGN_FILES+=("$IGN_FILE")
+done
+
+IGN_ARGS=()
+for IGN_FILE in "${IGN_FILES[@]}"; do
+    IGN_ARGS+=("--dest-ignition" "$IGN_FILE")
+done
+
 cd "$(dirname "$0")"/build
-
-podman run --rm -i \
-    quay.io/coreos/butane:release \
-    --pretty --strict < "$CONFIG" > config.ign
-
 [[ -f ignited.iso ]] && rm ignited.iso
 
 podman run --security-opt label=disable --pull=always --rm -v .:/data -w /data \
     quay.io/coreos/coreos-installer:release \
-    iso customize --dest-ignition config.ign --dest-device /dev/sda --dest-console=tty0 --dest-console ttyS0,115200n8 \
+    iso customize "${IGN_ARGS[@]}" --dest-device /dev/sda --dest-console=tty0 --dest-console ttyS0,115200n8 \
     -o ignited.iso fcos-custom-live-iso.x86_64.iso
